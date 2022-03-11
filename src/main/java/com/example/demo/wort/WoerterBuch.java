2d63885c1e0b227591769d8d411a33b1077863f3
TobiasSMI <41923750+TobiasSMI@users.noreply.github.com>
2022-03-11 23:21:09 +0100

    :art: Wort Config zu Wörterbuch umbenannt. Hier wird nicht wirklich etwas konfiguriert, sondern eher ... definiert.

--- src/main/java/com/example/demo/wort/WoerterBuch.java
+++ src/main/java/com/example/demo/wort/WoerterBuch.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class WortConfig {
+public class WoerterBuch {
 	
 	@Bean
 	CommandLineRunner commandLineRunner(WortRepository repository) {
